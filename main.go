50b4dcbbb2d9c368f1093890054c24fda140b4b3
jake <bigjake2s@gmail.com>
2012-08-13 15:04:14 -0400

    Added actual word processing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"github.com/zeebo/sbloom"
 	"hash/fnv"
@@ -8,12 +9,16 @@ import (
 	"os"
 )
 
-const probability = 10
+const (
+	probability = 10
+)
 
 var (
+	accept         = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
 	filters        map[string]sbloom.Filter
 	filterFilename string
 	searchMode     bool
+	wordbreaks     = []byte("\n\r ")
 )
 
 func init() {
@@ -27,7 +32,7 @@ func storeFile(f *os.File, err error) {
 	}
 	defer f.Close()
 
-	filters[f.Name()] = *sbloom.NewFilter(fnv.New64(), 10)
+	filter := sbloom.NewFilter(fnv.New64(), 10)
 
 	b := make([]byte, 1)
 	word := make([]byte, 64)
@@ -35,8 +40,18 @@ func storeFile(f *os.File, err error) {
 		if _, err := f.Read(b); err != nil {
 			break
 		}
+		switch {
+		// Store a word on a break
+		case bytes.Contains(wordbreaks, b):
+			filter.Add(word)
+			filter.Add(bytes.ToLower(word))
+			word = word[:0]
+		case bytes.Contains(accept, b):
+			word = append(word, b[0])
+		}
 		log.Printf("Letter/word: %s/%s", b, word)
 	}
+	filters[f.Name()] = *filter
 }
 
 func main() {
